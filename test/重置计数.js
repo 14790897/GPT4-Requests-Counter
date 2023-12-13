5a1f8c41a0896dd5730ac42e60cca4b7466374a1
Liuweiqing <121866954+14790897@users.noreply.github.com>
2023-12-13 11:08:18 +0800

    fix:使今日首次对话时间更准确

--- test/重置计数.js
+++ test/重置计数.js
@@ -1,7 +1,24 @@
 //下列代码需要在service worker的控制台执行
+//下面是模拟开始时间在三小时之前的代码
 chrome.storage.local.set(
   { startTime: Date.now() - 3 * 60 * 60 * 1000 * 2 },
   function () {
     console.log('startTime 设置为三小时前: ' + Date.now())
   }
 )
+
+//下面是模拟新的一天开始的代码
+
+// 创建当前日期的对象
+const currentDate = new Date();
+
+// 减去一天
+currentDate.setDate(currentDate.getDate() - 1);
+
+// 将日期转换为 YYYY-MM-DD 格式的字符串
+const lastUpdatedDate = currentDate.toISOString().split('T')[0];
+
+// 保存到 chrome.storage.local
+chrome.storage.local.set({ lastUpdatedDate: lastUpdatedDate }, function() {
+  console.log('lastUpdatedDate 设置为前一天: ' + lastUpdatedDate);
+});
