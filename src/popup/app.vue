f52b0849e42d866e1c8ee19863224b9030ea6dd9
Liuweiqing <121866954+14790897@users.noreply.github.com>
2024-01-01 17:16:34 +0800

    feat: github link

--- src/popup/app.vue
+++ src/popup/app.vue
@@ -10,12 +10,18 @@
 
   <RouterView></RouterView>
 
-  <!-- <footer
-    aria-label="Site Footer"
-    class="bg-gray-50"
-  >
-    Popup Footer
-  </footer> -->
+  <footer aria-label="Site Footer">
+    <div class="bg-gray-50 text-gray-700 px-4">
+      <div class="flex justify-between items-center">
+        <div>
+          <a href="https://github.com/14790897/GPT4-Requests-Counter/issues" class="mr-2" target="_blank">Help</a>
+        </div>
+        <div>
+          <a href="https://github.com/14790897/GPT4-Requests-Counter" class="mr-2" target="_blank"> give me a star</a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped></style>
