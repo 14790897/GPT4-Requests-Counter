ce6ecf509d0fb427d655e798b0f36c9497d4052d
14790897 <14790897abc@gmail.com>
2024-03-13 14:02:09 +0800

    test: 查看lastUpdatedDate时间

--- test/重置计数.js
+++ test/重置计数.js
@@ -7,19 +7,26 @@ chrome.storage.sync.set(
     console.log('startTime 设置为三小时前: ' + Date.now())
   }
 )
-
+//---------------------------------------
 //下面是模拟新的一天开始的代码
 
 // 创建当前日期的对象
-const currentDate = new Date();
+const currentDate = new Date()
 
 // 减去一天
-currentDate.setDate(currentDate.getDate() - 1);
+currentDate.setDate(currentDate.getDate() - 1)
 
 // 将日期转换为date.toDateString(); 格式的字符串
-const lastUpdatedDate = currentDate.toDateString();
+const lastUpdatedDate = currentDate.toDateString()
 
 // 保存到 chrome.storage.sync
-chrome.storage.sync.set({ lastUpdatedDate: lastUpdatedDate }, function() {
-  console.log('lastUpdatedDate 设置为前一天: ' + lastUpdatedDate);
-});
+chrome.storage.sync.set({ lastUpdatedDate: lastUpdatedDate }, function () {
+  console.log('lastUpdatedDate 设置为前一天: ' + lastUpdatedDate)
+})
+//---------------------------------------
+//查看时间
+chrome.storage.sync.get({ lastUpdatedDate: '默认日期' }, function (result) {
+  // 使用存储中的值或默认值
+  var lastUpdatedDate = result.lastUpdatedDate // 现在它在这个作用域内被正确定义了
+  console.log('lastUpdatedDate 的值是: ' + lastUpdatedDate)
+})
