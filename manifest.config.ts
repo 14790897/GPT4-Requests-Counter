8ee6a50bc215a8d45862b78cd01c597e1c9ad2c6
liuweiqing <liuweiqing147@gmail.com>
2024-06-24 21:35:31 +0800

    fix: 去除不需要的权限

--- manifest.config.ts
+++ manifest.config.ts
@@ -55,7 +55,7 @@ export default defineManifest(async (env) => ({
   ],
   // host_permissions: ['*://*/*'],
   options_page: 'src/options/index.html',
-  permissions: ['storage', 'downloads', 'tabs'],
+  permissions: ['storage', 'tabs'],
   web_accessible_resources: [
     {
       matches: ['*://*/*'],
